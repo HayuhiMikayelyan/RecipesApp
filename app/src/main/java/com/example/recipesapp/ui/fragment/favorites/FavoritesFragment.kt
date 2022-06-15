acba034858622e381755e10e4ab5e94dce0979a9
HayuhiM <hayuhi.m@tumo.org>
2022-06-15 21:27:44 +0300

    favorites(Categories)

--- app/src/main/java/com/example/recipesapp/ui/fragment/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/recipesapp/ui/fragment/favorites/FavoritesFragment.kt
@@ -8,10 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recipesapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recipesapp.databinding.FragmentFavoritesBinding
 import com.example.recipesapp.ui.fragment.home.main.DetailFragment
+import com.example.recipesapp.ui.models.CategoriesModel
 import com.example.recipesapp.ui.models.FoodModel
 import com.example.recipesapp.ui.recycler_adapters.FoodRecAdapter
 import com.google.firebase.auth.FirebaseAuth
@@ -20,8 +21,9 @@ import com.google.firebase.database.*
 class FavoritesFragment : Fragment() {
 private lateinit var adapter: FoodRecAdapter
     private lateinit var binding: FragmentFavoritesBinding
-    private lateinit var database: DatabaseReference
+    private lateinit var dbCategories: DatabaseReference
     private lateinit var foodList: MutableList<FoodModel>
+    private lateinit var favList: MutableList<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +33,7 @@ private lateinit var adapter: FoodRecAdapter
 
         binding.recFav.layoutManager = LinearLayoutManager(activity)
         foodList = mutableListOf()
+        favList = mutableListOf()
 
         return binding.root
     }
@@ -44,15 +47,72 @@ private lateinit var adapter: FoodRecAdapter
         binding.favProgress.visibility = ProgressBar.VISIBLE
         adapter = FoodRecAdapter(foodList)
         binding.recFav.adapter = adapter
-        database = FirebaseDatabase.getInstance().getReference("Drinks")
+        dbCategories = FirebaseDatabase.getInstance().getReference("Categories")
         val favourites = FirebaseDatabase.getInstance().getReference("Users")
             .child(FirebaseAuth.getInstance().currentUser!!.email.toString().replace(".", ""))
             .child("favoriteList")
-        database.addValueEventListener(object : ValueEventListener {
+
+        favourites.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (fav in snapshot.children){
+                    favList.add(fav.getValue(String::class.java)!!)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+        dbCategories.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(categories in snapshot.children){
+                    val category = categories.getValue(CategoriesModel::class.java)
+                    val dbFood = FirebaseDatabase.getInstance().getReference(category!!.name!!)
+                    dbFood.addValueEventListener(object : ValueEventListener{
+                        @SuppressLint("NotifyDataSetChanged")
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            for(foods in snapshot.children){
+                                val food = foods.getValue(FoodModel::class.java)
+                                if(food!!.name in favList){
+                                    foodList.add(food)
+                                    adapter.notifyDataSetChanged()
+                                }
+                            }
+
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                            Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
+                        }
+
+                    })
+                }
+                binding.favProgress.visibility = ProgressBar.GONE
+                adapter.onItemClick = {
+
+                    val bundle = Bundle()
+                    val fragment = DetailFragment()
+
+                    bundle.putParcelable("Food", it)
+                    fragment.arguments = bundle
+
+                    fragmentManager!!.beginTransaction()
+                        .replace(R.id.container, fragment)
+                        .addToBackStack(null)
+                        .commit()
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+   /*     database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
 
-                if (snapshot.exists()) {
-                    for (foodSnapshot in snapshot.children) {
+                for (foodSnapshot in snapshot.children) {
 
                         favourites.addValueEventListener(object : ValueEventListener {
                             @SuppressLint("NotifyDataSetChanged")
@@ -87,14 +147,13 @@ private lateinit var adapter: FoodRecAdapter
                             .addToBackStack(null)
                             .commit()
                     }
-                }
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Toast.makeText(activity, error.message, Toast.LENGTH_LONG).show()
             }
 
-        })
+        })*/
     }
 
 }
